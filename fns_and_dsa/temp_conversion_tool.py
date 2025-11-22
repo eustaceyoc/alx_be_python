3710fb13ea9d02beb09685d0e8da088cd2ed3215
Eustace Crentsil <crentsil.eustace25@gmail.com>
2025-11-22 17:20:06 +0000

    Corrected variables to match rubric.

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,11 +1,11 @@
-FAHRENHEIT_TO_CELSIUS = 5 / 9
-CELSIUS_TO_FAHRENHEIT = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return (celsius * CELSIUS_TO_FAHRENHEIT) + 32
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 temperature_input = float(input("Enter the temperature to convert: "))
 
