44d78cf9af08079c252f0868bbd1a2e9f8e09c75
Eustace Crentsil <crentsil.eustace25@gmail.com>
2025-11-22 16:42:35 +0000

    Add datetime task.

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(current_date)
+
+def calculate_future_date():
+    days_to_add = (int(input("Enter number of days to add: ")))
+    future_date = datetime.now() + timedelta(days=days_to_add)
+    future_date = future_date.strftime("%Y-%m-%d")
+    print(future_date)
+
+display_current_datetime()
+calculate_future_date()
