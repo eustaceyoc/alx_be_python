6a99e062ef3dcfbc35dc22f454239dfd75621c6a
Eustace Crentsil <crentsil.eustace25@gmail.com>
2025-11-22 15:40:23 +0000

    initial commit.

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+# arithmetic_operations.py
+
+def perform_operation(num1: float, num2: float, operation: str):
+
+    if operation == "add":
+        return num1 + num2
+
+    elif operation == "subtract":
+        return num1 - num2
+
+    elif operation == "multiply":
+        return num1 * num2
+
+    elif operation == "divide":
+        if num2 == 0:
+            return "DIVISION_BY_ZERO"
+        return num1 / num2
+
+    else:
+        return "INVALID_OPERATION"
