03a32bf1ed5bdb66deb3880f58a5a43ed9897bb8
Eustace Crentsil <crentsil.eustace25@gmail.com>
2025-11-22 16:47:22 +0000

    Fix input text for the third time.

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,7 +6,7 @@ def display_current_datetime():
     print(current_date)
 
 def calculate_future_date():
-    days_to_add = (int(input("Enter number of days to add to the current date:")))
+    days_to_add = (int(input("Enter the number of days to add to the current date:")))
     future_date = datetime.now() + timedelta(days=days_to_add)
     future_date = future_date.strftime("%Y-%m-%d")
     print(future_date)
